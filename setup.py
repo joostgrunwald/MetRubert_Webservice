1c55366303779d2adf8cc0cfbff286d99a8c81ba
Joost Grunwald <joostgrunwald2001@gmail.com>
2022-04-12 16:23:36 +0200

    updated setup

--- setup.py
+++ setup.py
@@ -14,14 +14,14 @@ def getreadme():
     return ""
 
 setup(
-    name = "Alpino Webservice",
-    version = "2.3", #make sure SYSTEM_VERSION in your service configuration is set to the same value!
-    author = "Maarten van Gompel", #adapt this
-    description = ("Alpino is a dependency parser for Dutch, developed in the context of the PIONIER Project Algorithms for Linguistic Processing, developed by Gertjan van Noord at the University of Groningen. This is the webservice for it. You can upload either tokenised or untokenised files (which will be automatically tokenised for you using ucto), the output will consist of a zip file containing XML files, one for each sentence in the input document."),
+    name = "metaphorclam",
+    version = "0.4", #make sure SYSTEM_VERSION in your service configuration is set to the same value!
+    author = "Joost Grunwald", #adapt this
+    description = ("MetRubert is a dutch metaphor precictor. You can upload either tokenised or untokenised files (which will be automatically tokenised for you using ucto), the output will consist of a zip file containing XML files, one for each sentence in the input document."),
     license = "GPL-3.0-only",
-    keywords = [ "nlp", "linguistics", "folia", "dependency parsing", "syntax"],
-    url = "https://github.com/proycon/alpino_clam_webservice",
-    packages=['alpino_webservice'],
+    keywords = [ "nlp", "linguistics", "MetRubert", "dependency parsing", "syntax"],
+    url = "https://github.com/joostgrunwald/MetRubert_webservice",
+    packages=['metaphorclam'],
     long_description=getreadme(),
     classifiers=[
         "Development Status :: 5 - Production/Stable",
@@ -37,7 +37,7 @@ setup(
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
     ],
-    package_data = {'alpino_webservice':['*.wsgi','*.yml','*.sh'] },
+    package_data = {'metaphorclam':['*.wsgi','*.yml','*.sh'] },
     include_package_data=True,
-    install_requires=['CLAM >= 3.1.4', 'FoLiA-tools', 'natsort'] #Alpino is also required but is an external dependency that setuptools can't handle, we specify it in codemeta-harvest.json for metadata harvesting purposes
+    install_requires=['CLAM >= 3.0', 'natsort'] #Alpino is also required but is an external dependency that setuptools can't handle, we specify it in codemeta-harvest.json for metadata harvesting purposes
 )
